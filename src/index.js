2964fa55620355bb89eff5b2418dec89732570bc
muhibk22 <m.muhibk22@gmail.com>
2025-07-13 18:25:13 +0500

    feat: make start screen

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import "./styles.css";
 import { generateBoards, setShipUI, makeMoveUI } from "./components/display";
 import Player from "./components/player";
 
-const player= new Player();
-const computer= new Player("computer");
-player.gameboard.placeRandomly();
-computer.gameboard.placeRandomly();
+// const player= new Player();
+// const computer= new Player("computer");
+// player.gameboard.placeRandomly();
+// computer.gameboard.placeRandomly();
 
-const playerBoard = document.querySelector(".player-board");
-const computerBoard= document.querySelector(".computer-board");
+// const playerBoard = document.querySelector(".player-board");
+// const computerBoard= document.querySelector(".computer-board");
 generateBoards(); 
-setShipUI(player.gameboard, playerBoard);
-setShipUI(computer.gameboard, computerBoard);
+// setShipUI(player.gameboard, playerBoard);
+// setShipUI(computer.gameboard, computerBoard);
 
-makeMoveUI(player, computer, computerBoard);
+// makeMoveUI(player, computer, computerBoard);
 
